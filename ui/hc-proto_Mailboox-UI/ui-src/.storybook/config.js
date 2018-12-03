7c9ad9a787b67c43520c8e1266ced5c6991549db
Viktor Zaunders <zaunders@gmail.com>
2018-12-03 21:15:30 +0100

    remove old ui stuff

--- ui/hc-proto_Mailboox-UI/ui-src/.storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/react'
-const req = require.context('../src/components', true, /story\.js$/)
-
-function loadStories () {
-  req.keys().forEach(req)
-}
-
-configure(loadStories, module)
